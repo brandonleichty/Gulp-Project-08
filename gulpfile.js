e30cdc1a51a3bec60a423681a0c7301ac2e8c802
Brandon Leichty <brandonl@me.com>
2017-05-09 22:38:13 -0500

    added .gitignore, gulpfile.js, package.json, and src files

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const del      = require('del');
+const gulp     = require('gulp');
+const sass     = require('gulp-sass');
+const concat   = require('gulp-concat');
+const uglify   = require('gulp-uglify');
+const rename   = require('gulp-rename');
+const connect  = require('gulp-connect');
+const imagemin = require('gulp-imagemin');
+const maps     = require('gulp-sourcemaps');
