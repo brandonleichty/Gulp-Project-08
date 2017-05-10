5f1aa52779670e28d0ab5c842a25ffe75649d8cd
Brandon Leichty <brandonl@me.com>
2017-05-10 00:24:23 -0500

    added images and serve tasks

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,65 @@ const imagemin = require('gulp-imagemin');
 const maps     = require('gulp-sourcemaps');
 
 
+
+// gulp scripts
 //
-// Dependancies
+// Concatenates, minifies, and copies all of the project’s JavaScript files into an all.min.js
+// file that is then copied to the dist/scripts folder.
+
+gulp.task('scripts', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+
+// gulp styles
 //
+// Compiles the project’s SCSS files into CSS, then concatenates and minifies into an all.min.css
+// file that is then copied to the dist/styles folder.
+
+gulp.task('styles', ['compileSass'], () => {
+  return gulp.src('src/sass/global.scss')
+  .pipe(concat('all.min.scss'))
+  .pipe(maps.init())
+  .pipe(sass())
+  .pipe(maps.write('./'))
+  .pipe(gulp.dest('dist/styles'));
+});
+
+
+// gulp compileSass
+//
+// Compiles all scss into css and places corresponding files into the src/css folder.
+
+gulp.task('compileSass', () => {
+  return gulp.src('src/sass/global.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('src/css'));
+});
+
+
+// gulp images
+//
+// Optimizes the size of the project’s JPEG and PNG files, and then copy those
+// optimized images to the dist/content folder.
+
+gulp.task('images', () => {
+    return gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/content'))
+});
+
+
+// gulp serve
+//
+// Serves the project using a local web server. Looks in the src folder for the index.html file.
+
+gulp.task('serve', () =>  {
+  connect.server({
+    root: 'src',
+    livereload: true
+  });
+});
